f11c6b7b64ec8e94c3845fad6c8e49fa55656484
Felifelps <felipefelipe23456@gmail.com>
2023-06-27 14:57:02 -0300

    asyncio in al

--- save.bat
+++ save.bat
@@ -2,32 +2,10 @@
 title Saving
 git init
 git add .
-
-call :asking
-
-:checking
-if %branch%==1 call :saving main
-if %branch%==2 call :saving async
-echo Invalid answer!
-pause
-cls
-call: asking
-EXIT /B 0
-
-:asking
 set /p "commit=Enter commit: "
-echo 1.main
-echo 2.async
-set /p "branch=Enter branch: "
-call :checking
-EXIT /B 0
-
-:saving
 git commit -m "%commit%"
-git push https://github.com/Felifelps/MilaFoods %~1
-echo %~1
+git push https://github.com/Felifelps/MilaFoods async
 echo Saved
 pause
-exit
-EXIT /B 0
+
 
